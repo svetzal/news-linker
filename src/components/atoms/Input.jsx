73dcc3db4bf9609e42f09832f4c0cb574ccf2faa
Stacey Vetzal <stacey@vetzal.com>
2025-04-16 13:04:26 -0400

    style: update input color to black for better visibility

--- src/components/atoms/Input.jsx
+++ src/components/atoms/Input.jsx
@@ -17,12 +17,12 @@ const Input = ({
   className = '',
   ...props
 }) => {
-  const baseClasses = 'w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2';
+  const baseClasses = 'text-black w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2';
   const stateClasses = error
     ? 'border-red-300 focus:border-red-300 focus:ring-red-200'
     : 'border-gray-300 focus:border-canadian-red focus:ring-canadian-red/20';
   const disabledClass = disabled ? 'bg-gray-100 text-gray-500 cursor-not-allowed' : '';
-  
+
   const inputClasses = `${baseClasses} ${stateClasses} ${disabledClass} ${className}`;
 
   return (
